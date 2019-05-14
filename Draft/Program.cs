f39b5f4502fd2e1db098b69259a1f4100b8c0835
Xristos Metaxas <xristos.metaxas@xprs.local>
2019-05-14 16:33:54 +0300

    Add project files.

---
+++ Draft/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print to the screen
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            // keep open the console window
+
+            Console.ReadLine();
+        }
+    }
+}
