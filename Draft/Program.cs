5ae282ea57d2c84a89c41db83d0dd43c5b091b3f
metxri1@gmail.com <metxri1@gmail.com>
2019-05-15 14:18:47 +0300

    Data Types Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -10,7 +10,16 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            
+            // Data Types
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            // for decimal double float decimal
+            double gpa = 3.2;
+            bool isMale = true;
+
+
+            Console.WriteLine("Hello");
 
             Console.ReadLine();
         }
