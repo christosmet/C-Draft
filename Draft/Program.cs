855e2f1627bce652ab01757dbd3b05913ce0f14a
metxri1@gmail.com <metxri1@gmail.com>
2019-05-15 14:45:43 +0300

    Working With Strings Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -11,6 +11,26 @@ namespace Giraffe
         static void Main(string[] args)
         {
             // Working With Strings
+            string phrase = "Giraffe Academy";
+
+            Console.WriteLine("Giraffe\nAcademy");
+
+            Console.WriteLine("\"Giraffe\"Academy");
+
+            Console.WriteLine(phrase);
+
+            Console.WriteLine("Giraffe Academy" + " is cool");
+
+            Console.WriteLine(phrase.Length);
+
+            // string Methods
+
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.ToLower());
+            Console.WriteLine(phrase.Contains("Academy")); //returns true false
+            Console.WriteLine(phrase[2]);
+            Console.WriteLine(phrase.IndexOf("Academy")); //Where it starts index
+            Console.WriteLine(phrase.Substring(8, 3)); // prints from the index and after
 
 
             Console.ReadLine();
