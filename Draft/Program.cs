ecd6ba54b0f2e9b642c32b28d2ccb480a18556ff
metxri1@gmail.com <metxri1@gmail.com>
2019-05-15 16:25:18 +0300

    Building a Calculator Section Start

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -10,14 +10,14 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            // Print to the screen
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            // Building a Calculator
+
+
+
+
+
 
             // keep open the console window
-
             Console.ReadLine();
         }
     }
