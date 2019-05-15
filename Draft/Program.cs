aaafc005426f92be3ffe709ad1a27a11caa494a9
metxri1@gmail.com <metxri1@gmail.com>
2019-05-15 21:29:23 +0300

    Calculator Build Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -11,10 +11,15 @@ namespace Giraffe
         static void Main(string[] args)
         {
             // Building a Calculator
+            // int num = Convert.ToInt32("45"); // Convert string to integer
+            // Console.WriteLine(num);
 
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
-
-
+            Console.WriteLine(num1 + num2);
 
 
             // keep open the console window
