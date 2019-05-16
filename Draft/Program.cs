75a24fbbaff846a76f058e884d1970eb2ff9b316
metxri1@gmail.com <metxri1@gmail.com>
2019-05-16 14:20:58 +0300

    Methods Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -11,13 +11,17 @@ namespace Giraffe
         static void Main(string[] args)
         {
             // Methods
-
-
-
-
+            // Call A Method
+            SayHi("Christos", 33);
+            SayHi("Tom", 12);
             // keep open the console window
-
             Console.ReadLine();
+        }
+
+        // Method Structure
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age + " years old");
         }
     }
 }
