671ddc4861e9c2f8c0a15c27efb4971353c96f57
metxri1@gmail.com <metxri1@gmail.com>
2019-05-16 12:56:21 +0300

    Arrays Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -11,9 +11,20 @@ namespace Giraffe
         static void Main(string[] args)
         {
             // Arrays
+            int[] luckyNumbers = {4, 8, 15, 16, 23, 42};
+            string[] friends = new string[5]; //new string[number of values]
+
+            // Change a value
+            luckyNumbers[1] = 900;
+            friends[0] = "Christos";
+            friends[1] = "Jim";
+            friends[2] = "Kelly";
+            friends[3] = "Manolis";
+            friends[4] = "Kostas";
 
 
-
+            Console.WriteLine(luckyNumbers[1]);
+            Console.WriteLine(friends[4]);
 
             // keep open the console window
             Console.ReadLine();
