1e76d6fd86cfee464037ccd4e22f32fa236367c1
metxri1@gmail.com <metxri1@gmail.com>
2019-05-16 12:39:49 +0300

    Mad Libs Game Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -11,9 +11,19 @@ namespace Giraffe
         static void Main(string[] args)
         {
             // build Mad Libs Game
-            Console.WriteLine("Roses are red");
-            Console.WriteLine("Violets are blue");
-            Console.WriteLine("I Love you");
+
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
 
 
             // keep open the console window
