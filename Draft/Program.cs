f6b90ed1143f9ac747790adc0314fc867bd00725
metxri1@gmail.com <metxri1@gmail.com>
2019-05-17 23:02:25 +0300

    If Statements Section2 End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -11,10 +11,25 @@ namespace Giraffe
         static void Main(string[] args)
         {
             // More If Statements 
-
+            Console.WriteLine(GetMax(20,10,4));
 
             // keep open the console window
             Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            if(num1 >= num2 && num1 > num3)
+            {
+                result = num1;
+            } else if (num2 >= num1 && num2 >= num3) 
+            {
+                result = num2;
+            } else 
+            {
+                result = num3;
+            }   
         }
     }
 }
