bcbdc4c3b86b5f176bc2c9a3519d0d08d27cdf1d
metxri1@gmail.com <metxri1@gmail.com>
2019-05-17 23:04:04 +0300

    Building a Better Calculator Start

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -10,11 +10,7 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            // Print to the screen
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            // Building a Better Calculator
 
             // keep open the console window
 
