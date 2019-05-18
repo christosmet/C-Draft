d0e5740ffc0caa9fabe16e61e55c7dfb40b66028
metxri1@gmail.com <metxri1@gmail.com>
2019-05-18 19:35:43 +0300

    Comments Section Start

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -10,14 +10,12 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            // Print to the screen
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            // Comments
+
+
+
 
             // keep open the console window
-
             Console.ReadLine();
         }
     }
