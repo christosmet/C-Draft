bca964ab3b8a899caa575dc0fb062ded11993c6f
metxri1@gmail.com <metxri1@gmail.com>
2019-05-18 19:34:31 +0300

    2d Arrays Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -11,8 +11,14 @@ namespace Giraffe
         static void Main(string[] args)
         {
             // 2d Arrays
+            int[,] numberGrid = {{1, 2}, {3, 4}, {5, 6}}; 
+            
+            // if array values are unknown
+            int[,] myArray = new int[2, 3]; // ex. has 2 elements and each of this have 3 elements inside
 
 
+
+            Console.WriteLine(numberGrid[0, 0]);
 
             // keep open the console window
             Console.ReadLine();
