562b1ec18cea0a5df623c78372ea54f4dabd90ba
metxri1@gmail.com <metxri1@gmail.com>
2019-05-18 18:13:39 +0300

    Build Guess Game Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -10,14 +10,39 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            // Print to the screen
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            // Building a Guessing Game
 
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while(guess != secretWord && !outOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;    
+                }
+                else 
+                {
+                    outOfGuesses = true;
+                }
+                
+            }
+            if(outOfGuesses)
+            {
+                Console.WriteLine("You Lose!");
+            } 
+            else 
+            {
+                Console.WriteLine("You Win!");
+            } 
+            
+            
             // keep open the console window
-
             Console.ReadLine();
         }
     }
