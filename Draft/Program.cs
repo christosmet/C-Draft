f9e1160ae5c5517c0e0e9580d0ee177981544e29
metxri1@gmail.com <metxri1@gmail.com>
2019-05-18 18:37:03 +0300

    For Loops Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -11,7 +11,12 @@ namespace Giraffe
         static void Main(string[] args)
         {
             // For Loops
-
+            int[] luckyNumbers = {4, 8, 15, 16, 23, 42};
+            for(int i = 0; i < luckyNumbers.Length; i++)
+            {
+                
+                Console.WriteLine(luckyNumbers[i]);
+            }
 
 
             // keep open the console window
