a2ed0de8f15dcdcd62cdc37ff88bd273dab71ed5
metxri1@gmail.com <metxri1@gmail.com>
2019-05-18 20:00:05 +0300

    Exception Handling Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -12,7 +12,36 @@ namespace Giraffe
         {
             // Exception Handling
 
+            // Add risky code that maybe throw exception message
+            try 
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine(num1 / num2);
+            }
+            /*
+            catch(Exception e) // Throw an error message and program doesn't terminate
+            {
+                Console.WriteLine(e.Message);
+
+            }*/
+            // can specify a specific type of exception
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            /* code in finaly will be executed no matter what (optional)
+            finally
+            {
+                
+            } */
 
             // keep open the console window
             Console.ReadLine();
