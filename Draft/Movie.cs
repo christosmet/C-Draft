936b3ee6b6c100a518728983adca01dfea745642
metxri1@gmail.com <metxri1@gmail.com>
2019-05-19 14:11:51 +0300

    Getters & Setters Section End

---
+++ Draft/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating; // private can be accessed only by the Movie Class
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating; // SET the value by calling the setter method
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set 
+            {
+                if(value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else 
+                {
+                    rating = "NR";
+                }
+            }
+        }
+
+
+    }
+}
--- Draft/Program.cs
+++ Draft/Program.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Giraffe
+namespace Draft
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Getters & Setters
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "Dog");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+
+            Console.WriteLine(avengers.Rating);
 
             // keep open the console window
             Console.ReadLine();
