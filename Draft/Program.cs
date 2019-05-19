73cfc3192301788c9df961bce916cd6f51a73e89
metxri1@gmail.com <metxri1@gmail.com>
2019-05-19 16:50:19 +0300

    Static Class Attributes Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -4,20 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Giraffe
+namespace Draft
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Print to the screen
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            // Static Class Attributes
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(kashmir.getSongCount());
 
             // keep open the console window
-
             Console.ReadLine();
         }
     }
---
+++ Draft/Song.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draft
+{
+    class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+        // Static variable the same for all objects that will be created from this class
+        public static int songCount = 0;
+
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            // Every time a Song Object creates increment song Count by 1
+            songCount++;
+        }
+
+        public int getSongCount()
+        {
+            return songCount;
+        }
+    }
+}
