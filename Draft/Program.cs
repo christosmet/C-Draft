89cf6391c84744d650ad53cd7d7e70573b20b95a
metxri1@gmail.com <metxri1@gmail.com>
2019-05-19 12:23:44 +0300

    Classes & Objects Section End

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -4,16 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Giraffe
+namespace Draft
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Classes & Objects
+            // Create a Book
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
 
+            Book book2 = new Book();
+            book2.title = "Lord Of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
 
-
+            Console.WriteLine(book2.author);
 
             // keep open the console window
             Console.ReadLine();
